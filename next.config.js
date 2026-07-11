28678cb575afa4d97debd614a61461ee15687f17
azumimicucu <zidniganzs@gmail.com>
2026-07-11 19:17:43 +0700

    rilis 4

--- next.config.js
+++ next.config.js
@@ -1,5 +1,6 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
+  serverExternalPackages: ["@libsql/client"],
   images: {
     remotePatterns: [
       { protocol: "https", hostname: "i.ibb.co" },
